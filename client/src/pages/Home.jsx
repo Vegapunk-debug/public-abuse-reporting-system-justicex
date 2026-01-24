64d8edb720e2d5f67f9e5321a5beb0c07861ff8f
Rohit Nair P <rohitnairmuttathethu@gmail.com>
2026-01-25 00:00:05 +0530

    footer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSlider from '../components/HeroSlider'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div>
       <HeroSlider />
+      <Footer />
     </div>
   )
 }
